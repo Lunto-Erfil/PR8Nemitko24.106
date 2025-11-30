3fce4c30d92c82de1d0b1f0e6da6eeb96db79cdf
Dell <akkuma261@gmail.com>
2025-11-30 22:34:52 +0700

    Push

---
+++ app/src/main/java/com/example/pr8nemitko24106/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.pr8nemitko24106.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun PR8Nemitko24106Theme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
